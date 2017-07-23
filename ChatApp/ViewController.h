c42a1efbc17167b72e48bea31da200632a950b8d
pradeep gunasekaran <pradeepgunasekaran@pradeeps-macbook-pro.local>
2017-07-23 15:09:45 -0400

    Initial Commit

---
+++ ChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatApp
+//
+//  Created by pradeep gunasekaran on 7/23/17.
+//  Copyright © 2017 pradeep gunasekaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChatApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChatApp
+//
+//  Created by pradeep gunasekaran on 7/23/17.
+//  Copyright © 2017 pradeep gunasekaran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
